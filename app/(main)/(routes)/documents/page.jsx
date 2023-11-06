d8e4b64fd88547222fe9ba0b26915fc647e609f5
hjali7 <hjikingali@gmail.com>
2023-11-06 16:46:23 +0330

    add navside and basic set up for documents page

--- app/(main)/(routes)/documents/page.jsx
+++ app/(main)/(routes)/documents/page.jsx
@@ -1,7 +1,22 @@
+'use client'
 
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
 
 export default function DocumentsPage () {
+
+    const {user} = useUser()
+    
     return (
-        <>this is documents</>
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+
+            <Image height='300' width='300' alt="empty"  src='/empty.png' className="dark:hidden" />
+            <Image height='300' width='300'  alt="empty" src='/empty-dark.png' className="hidden dark:block" />
+
+            <h2 className="text-lg font-meduim">
+                Welcome To {user?.firstName}&apos;s Aotion
+            </h2>
+            
+        </div>
     )
 }
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,138 @@
+'use client'
 
+import { ElementRef, useRef, useState , useEffect} from "react"
+import { usePathname } from "next/navigation"
+
+import { ChevronsLeft, MenuIcon } from "lucide-react"
+import { useMediaQuery } from "usehooks-ts"
+
+import { cn } from "@/lib/utils"
 
 export default function Navigation () {
+    const pathname = usePathname()
+    const isMobile = useMediaQuery('(max-width: 768px)')
+
+    const isResizingRef = useRef(false)
+    const sidebarRef = useRef<ElementRef<'aside'>>(null)
+    const navbarRef = useRef<ElementRef<'div'>>(null)
+
+    const [ isResetting , setIsResetting ] = useState(false)
+    const [ isCollapsed , setIsCollapsed ] = useState(isMobile)
+
+    useEffect(() => {
+      if(isMobile) {
+        Collapse()
+      }else {
+        ResetWidth()
+      }
+    }, [isMobile])
+
+    useEffect(() => {
+        if(isMobile) {
+            Collapse()
+        }
+    }, [pathname , isMobile])
+    
+    
+
+    const handleMouseDown = (event : React.MouseEvent<HTMLElement , MouseEvent>) => {
+        event.preventDefault()
+        event.stopPropagation()
+
+        document.addEventListener('mousemove', handleMouseMove)
+        document.addEventListener("mouseup" , handleMouseUp)
+        isResizingRef.current = true ;
+    }
+
+    const handleMouseMove = (event : MouseEvent) => {
+        if(!isResizingRef.current) return ;
+
+        let NewSize = event.clientX
+
+        if(NewSize > 480) {
+            NewSize = 480
+        }else if (NewSize < 240) {
+            NewSize = 240
+        }
+
+        if(sidebarRef.current && navbarRef.current) {
+            sidebarRef.current.style.width = `${NewSize}px`
+            navbarRef.current.style.setProperty('left' , `${NewSize}px`)
+            navbarRef.current.style.setProperty('width' , `calc(100% - ${NewSize}px)`)
+        }
+    }
+
+    const handleMouseUp = () => {
+        document.removeEventListener('mousemove', handleMouseMove)
+        document.removeEventListener('mouseup' , handleMouseUp)
+        isResizingRef.current = false ;
+    }
+
+    const ResetWidth = () => {
+        if(sidebarRef.current && navbarRef.current) {
+            setIsCollapsed(false)
+            setIsResetting(true)
+
+            sidebarRef.current.style.width =  isMobile ? " 100% ": "480px" ;
+            navbarRef.current.style.setProperty('width' , isMobile ? '0' : 'calc(100%-480px)')
+            navbarRef.current.style.setProperty('left' , isMobile ? '100%' : '480px')
+            
+            setTimeout(() => {
+                setIsResetting(false)
+            }, 300);
+        }        
+    }
+
+    const Collapse = () => {
+        if(sidebarRef.current && navbarRef.current) {
+            setIsCollapsed(true)
+            setIsResetting(true)
+
+            sidebarRef.current.style.width = isMobile ? '0' : '240px'
+            navbarRef.current.style.setProperty('width' , isMobile ? '100%' : 'calc(100%-240px)')
+            navbarRef.current.style.setProperty('left' , isMobile ? '0' : '240px')
+            
+            setTimeout(() => {
+                setIsResetting(false)
+            }, 300);
+        }
+    }
+    
+    
     return (
-        <> this is navbar</>
+        <>
+            <aside
+             ref={sidebarRef}
+             className={cn("group/sidebar h-full bg-secondary overflow-y-auto relative flex flex-col z-[99999] w-60",
+             isMobile && 'w-0',
+             isResetting && 'transition-all ease-in-out duration-300'
+             )}>
+                <div role="button" onClick={Collapse} className={cn("h-6 w-6 text-muted-foreground absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition hover:bg-neutral-100 dark:hover:bg-neutral-600 rounded-sm",
+                 isMobile && 'opacity-100')}>
+                    <ChevronsLeft className="h-6 w-6"/>
+                </div>
+                
+                <div>
+                    <p>action itmes</p>    
+                </div>                
+
+                <div className="mt-4">
+                    <p>documents</p>
+                </div>
+                <div
+                 onMouseDown={handleMouseDown}
+                 onClick={ResetWidth}
+                 className="opacity-0 group-hover/sidebar:opacity-100 h-full absolute w-1 bg-primary/10 transition cursor-ew-resize top-0 right-0" />
+            </aside>
+
+            <div className={cn('absolute top-0 left-60 z-[99999] w-[calc(100%-240px)]',
+             isResetting && 'transition-all ease-in-out duration-300',
+             isMobile && 'left-0 w-full'
+            )} ref={navbarRef} >
+                <nav className="bg-transparent px-3 py-2 w-full">
+                    {isCollapsed && <MenuIcon role="butotn" onClick={ResetWidth} className="h-6 w-6 text-muted-foreground" />}
+                </nav>
+            </div>
+        </>
     )
 }
